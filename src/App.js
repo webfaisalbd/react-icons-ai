693d1964e400cbffd7ca3f541aa86f6bbe60cd7c
webfaisalbd <web.faisal.bd@gmail.com>
2022-11-20 21:05:38 +0600

    react icons added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import './style.css'
+import './style.css';
+import { CiLocationOn } from "react-icons/ci";
+import { FaPhoneSquareAlt } from "react-icons/fa";
+import { HiMail } from "react-icons/hi";
 
 function App() {
   return (
@@ -9,17 +12,19 @@ function App() {
           <h4 className="cardDesignation">Frontend Web Developer</h4>
         </div>
         <div className="contactCard">
-          <div className="location">
-            <p>Dhaka</p>
-            <p>icon</p>
-          </div>
-          <div className="mobile">
-            <p>+8801685477114</p>
-            <p>icon</p>
-          </div>
-          <div className="email">
-            <p>web.faisal.bd@gmail.com</p>
-            <p>icon</p>
+          <div className="contactRight">
+            <div className="location">
+              <p>Dhaka</p>
+              <p> <CiLocationOn className='icon' /> </p>
+            </div>
+            <div className="mobile">
+              <p>+8801685477114</p>
+              <p> <FaPhoneSquareAlt className='icon' /> </p>
+            </div>
+            <div className="email">
+              <p>web.faisal.bd@gmail.com</p>
+              <p> <HiMail className='icon' /> </p>
+            </div>
           </div>
         </div>
       </div>
